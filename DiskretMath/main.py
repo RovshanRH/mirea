d1daf4f17c37b5112b6694215004e62f127dcb8f
RovshanRH <rovshanbro@gmail.com>
2025-09-07 13:37:56 +0300

    added sorted dicts

--- DiskretMath/main.py
+++ DiskretMath/main.py
@@ -1,6 +1,10 @@
 import re
 import math
 
+
+H1, H2 = 0, 0
+P1, P2 = [], []
+
 with open("Assets/text.txt", mode="r+") as f:
     with open("Assets/new_text.txt", mode="w") as file:
         for line in f.readlines():
@@ -11,6 +15,7 @@ with open("Assets/text.txt", mode="r+") as f:
 with open("Assets/new_text.txt", mode="r") as f:
     data = {}
     lines = f.readline()
+    textLength = len(lines)
     for i in lines:
         if i not in data:
             data[i] = 1
@@ -24,21 +29,22 @@ with open("Assets/new_text.txt", mode="r") as f:
         else:
             data_2[lines[i]+lines[i+1]] += 1
 
-    H1, H2 = 0, 0
-    P1, P2 = [], []
 
     for i in data:
-        P1.append(data.get(i)/len(lines))
+        P1.append(data.get(i)/textLength)
     for i in data_2:
-        P2.append(data_2.get(i)/len(lines))
+        P2.append(data_2.get(i)/textLength)
 
     for i in P1:
         H1 += (-1)* i * math.log(i, 2)
     for i in P2:
         H2 += (-1)* i * math.log(i, 2)
 
-print("Частота появления однобуквенных сочетаний: ", data)
-print("Частота появления двухбуквенных сочетаний: ", data_2)
+    sortedData = sorted(data.items(), key=lambda item : item[1], reverse=True)
+    sortedData_2 = sorted(data_2.items(), key=lambda item : item[1], reverse=True)
+
+print("Частота появления однобуквенных сочетаний: ", sortedData)
+print("Частота появления двухбуквенных сочетаний: ", sortedData_2)
 
 print(f"Энтропия, приходящаяся в среднем на 1 букву: {H1},\nНа 2 буквы: {H2} ")
 code_length = math.ceil(math.log(len(data), 2))
