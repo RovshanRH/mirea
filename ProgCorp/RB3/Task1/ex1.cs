f303d5c46d1c4c32c3529e419fcd3db8d773691a
RovshanRH <rovshanbro@gmail.com>
2025-10-29 20:42:31 +0300

    RB3 is done

---
+++ ProgCorp/RB3/Task1/ex1.cs
@@ -0,0 +1,7 @@
+﻿
+static string func(int n)
+{
+    if (n == 0) return "";
+    return  $"{(n%10)}" + $"{func(n/10)}";
+}
+Console.WriteLine($"{func(123456)}");
---
+++ ProgCorp/RB3/Task2/ex2.cs
@@ -0,0 +1,12 @@
+﻿static int accerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return accerman(m - 1, 1);
+    if (m > 0 && n > 0) return accerman(m - 1, accerman(m, n - 1));
+    return 1;
+}
+Console.WriteLine("Ввод: m=");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("n=");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Вывод: A(m,n)={accerman(m, n)}");
